d8cf3f5b2c34fe607290d1f6494a786a51ca6c41
930294112 <930294112@qq.com>
2017-10-16 20:35:56 +0800

    又添加注释 作者;高鹏

--- src/com/lanou/action/HelloAction.java
+++ src/com/lanou/action/HelloAction.java
@@ -9,6 +9,8 @@ public class HelloAction {
      * 访问权限必须是public, Struts才能访问到该方法
      * 返回类型必须是String, 代表该动作执行之后的结果
      */
+
+    //又来加注释啦
     public String say(){
         System.out.println("你好,蓝鸥");
         return "success";
